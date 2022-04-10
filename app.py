a1a881bc70d2b9b1c47b5ffb577ef9f5235fe412
Stoyan <sgrozev02@gmail.com>
2022-04-10 02:48:40 +0200

    Can't get any better bug fixes code refactoring final additions added

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ board.set_pin_mode_digital_output(YELLOW_LED)
 board.set_pin_mode_digital_output(BUZZER)
 number=0
 app = Flask(__name__)
-#functions
+#initialization
 order=[]
 with open('CSV/Customers.csv', 'r', newline='') as file: 
         reader = csv.DictReader(file)
@@ -31,6 +31,61 @@ with open('CSV/Customers.csv', 'r', newline='') as file:
             number=int(order[-1])
         if not number:
             number=0
+
+
+#functions
+
+def return_orders_from_row():
+    listOfOrder=[]
+    with open('CSV/Customers.csv', 'r', newline='') as file: 
+        reader = csv.DictReader(file)
+        for row in reader:
+            ordersFromRow=row['orderID']
+            listOfOrder.append(int(ordersFromRow))
+    return listOfOrder
+
+def check_input_value():
+    row = request.form['idRow']
+    if (row):
+        value=int(row)-1
+    else:
+        value=900000000000
+    return int(value)    
+
+def play_led_and_buzzer(row):
+    board.digital_write(GREEN_LED, ON)
+    board.digital_write(BUZZER, ON)
+    time.sleep(0.1)
+    board.digital_write(BUZZER, OFF)
+    board.digital_write(GREEN_LED, OFF)
+    time.sleep(0.1)
+    board.digital_write(GREEN_LED, ON)
+    time.sleep(0.1)
+    board.digital_write(GREEN_LED, OFF)
+    board.displayShow(row)
+    time.sleep(0.1)
+    board.displayShow("0000")
+
+def write_to_csv_if_con_true(value):
+    
+    df = pd.read_csv("CSV/Customers.csv") #IMPORTANT dont remove this because thid df because it will make the checking unusable after reopenning
+    state =request.form['ChangeState']
+    if state == "Checked":
+        play_led_and_buzzer(value) 
+    listOfOrder=return_orders_from_row()
+    if (listOfOrder):
+        if listOfOrder[-1]>=value and value>=0:
+            df.at[value, 'state']=state
+            df.to_csv("CSV/Customers.csv", index=False)
+
+def read_order_state():
+    with open('CSV/Customers.csv', 'r', newline='') as file: 
+        reader = csv.DictReader(file)
+        state=[]
+        for row in reader:
+            orders=row['state']
+            state.append(orders)
+    return state
 
 def delete_checked_orders():
     df = pd.read_csv("CSV/Customers.csv")
@@ -63,6 +118,14 @@ def values_from_forms(pizzaName):
             comment="None"
         combine=[auto_increment(), pizzaType, firstChoice, secondChoice, thirdChoice, forthChoice, quantity, comment, unChecked]
         return combine
+
+
+def write_pizza_type_to_csv(pizzaName):
+    with open('CSV/Customers.csv','a') as inFile:
+            writer = csv.writer(inFile)
+            writer.writerow(values_from_forms(pizzaName)) 
+    board.digital_write(YELLOW_LED, ON)        
+
 
 
 def readings_from_file():
@@ -123,10 +186,7 @@ def main_page():
 @app.route("/original_index", methods =['GET','POST'])
 def original_index():
     if request.method == 'POST':
-        with open('CSV/Customers.csv','a') as inFile:
-            writer = csv.writer(inFile)
-            writer.writerow(values_from_forms("original")) 
-        board.digital_write(YELLOW_LED, ON)
+        write_pizza_type_to_csv("original")
         return redirect(url_for('main_page'))
     elif request.method=="GET":
         return render_template("original.html")
@@ -135,10 +195,7 @@ def original_index():
 @app.route("/cheesy_index", methods =['GET','POST'])
 def cheesy_index():
     if request.method == 'POST':
-        with open('CSV/Customers.csv','a') as inFile:
-            writer = csv.writer(inFile)
-            writer.writerow(values_from_forms("cheesy")) 
-        board.digital_write(YELLOW_LED, ON)
+        write_pizza_type_to_csv("cheesy")
         return redirect(url_for('main_page'))
     elif request.method=="GET":
         return render_template("cheesy.html")
@@ -147,10 +204,7 @@ def cheesy_index():
 @app.route("/angus_index", methods =['GET','POST'])
 def angus_index():
     if request.method == 'POST':
-        with open('CSV/Customers.csv','a') as inFile:
-            writer = csv.writer(inFile)
-            writer.writerow(values_from_forms("angus")) 
-        board.digital_write(YELLOW_LED, ON)
+        write_pizza_type_to_csv("angus")
         return redirect(url_for('main_page'))
     elif request.method=="GET":
         return render_template("blackangus.html")
@@ -160,69 +214,29 @@ def angus_index():
 @app.route("/mama_index", methods =['GET','POST'])
 def mama_index():
     if request.method == 'POST':
-        with open('CSV/Customers.csv','a') as inFile:
-            writer = csv.writer(inFile)
-            writer.writerow(values_from_forms("mamaMia"))
-        board.digital_write(YELLOW_LED, ON)
+        write_pizza_type_to_csv("mamaMia")
         return redirect(url_for('main_page'))
     elif request.method=="GET":
         return render_template("mamaMia.html")
-        
-
-
-    
+         
 
 @app.route("/cook_index", methods =['GET','POST'])
 def cook_index():
-    listOfOrder=[]
-    with open('CSV/Customers.csv', 'r', newline='') as file: 
-        reader = csv.DictReader(file)
-        for row in reader:
-            ordersFromRow=row['orderID']
-            listOfOrder.append(ordersFromRow)
     if request.method=="POST":
-        df = pd.read_csv("CSV/Customers.csv") #IMPORTANT dont remove this because thid df because it will make the checking unusable after reopenning
-        row = request.form['idRow']
-        state =request.form['ChangeState']
-        if state == "Checked":
-          board.digital_write(GREEN_LED, ON)
-          board.digital_write(BUZZER, ON)
-          time.sleep(0.1)
-          board.digital_write(BUZZER, OFF)
-          board.digital_write(GREEN_LED, OFF)
-          time.sleep(0.1)
-          board.digital_write(GREEN_LED, ON)
-          time.sleep(0.1)
-          board.digital_write(GREEN_LED, OFF)
-          board.displayShow(row)
-          time.sleep(0.1)
-          board.displayShow("0000")
-        if (row):
-            value=int(row)-1
-        else:
-            value=900000000000
-        if (listOfOrder):
-            if int(listOfOrder[-1])>=int(value) and int(value)>=0:
-                df.at[int(value), 'state']=state
-                df.to_csv("CSV/Customers.csv", index=False)
+        row=check_input_value()
+        write_to_csv_if_con_true(row)
         return redirect(url_for('cook_index', orders=readings_from_file()[0], pizzaTypes=readings_from_file()[1], sizes=readings_from_file()[2], toppings1=readings_from_file()[3], toppings2=readings_from_file()[6], toppings3=readings_from_file()[7], quantities=readings_from_file()[8], comments=readings_from_file()[4],checkboxes=readings_from_file()[5]))
     elif request.method=="GET":
         delete_checked_orders()
-        with open('CSV/Customers.csv', 'r', newline='') as file: 
-            reader = csv.DictReader(file)
-            state=[]
-            for row in reader:
-                orders=row['state']
-                state.append(orders)
-            result = all(element == "Checked" for element in state)
-            print(result)
-            if result:
-                    board.digital_write(YELLOW_LED, OFF)
-                    board.digital_write(RED_LED, ON)  
-            else:
-                    board.digital_write(YELLOW_LED, ON)
-                    board.digital_write(RED_LED, OFF)
-                    return render_template("cookDisplay.html", orders=readings_from_file()[0], pizzaTypes=readings_from_file()[1], sizes=readings_from_file()[2], toppings1=readings_from_file()[3], toppings2=readings_from_file()[6], toppings3=readings_from_file()[7], quantities=readings_from_file()[8], comments=readings_from_file()[4],checkboxes=readings_from_file()[5])
+        state=read_order_state()
+        result = all(element == "Checked" for element in state)
+        if result:
+                board.digital_write(YELLOW_LED, OFF)
+                board.digital_write(RED_LED, ON)  
+        else:
+                board.digital_write(YELLOW_LED, ON)
+                board.digital_write(RED_LED, OFF)
+                return render_template("cookDisplay.html", orders=readings_from_file()[0], pizzaTypes=readings_from_file()[1], sizes=readings_from_file()[2], toppings1=readings_from_file()[3], toppings2=readings_from_file()[6], toppings3=readings_from_file()[7], quantities=readings_from_file()[8], comments=readings_from_file()[4],checkboxes=readings_from_file()[5])
         return render_template("cookDisplay.html", orders=readings_from_file()[0], pizzaTypes=readings_from_file()[1], sizes=readings_from_file()[2], toppings1=readings_from_file()[3], toppings2=readings_from_file()[6], toppings3=readings_from_file()[7], quantities=readings_from_file()[8], comments=readings_from_file()[4],checkboxes=readings_from_file()[5])
 
 
